78ea7347758c93e4aea996fbf2cd824b33bae65a
delindell <daviselindell@gmail.com>
2020-03-28 10:03:51 -0500

    wip

--- src/javascripts/helpers/data/pinData.js
+++ src/javascripts/helpers/data/pinData.js
@@ -3,6 +3,21 @@ import apiKeys from '../apiKeys.json';
 
 const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
-const getPins = () => axios.get(`${baseUrl}/pins.json`);
+const getPins = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/pins.json`)
+    .then((response) => {
+      const demPins = response.data;
+      const pins = [];
+      Object.keys(demPins).forEach((pinId) => {
+        demPins[pinId].id = pinId;
+        pins.push(demPins[pinId]);
+      });
+      resolve(pins);
+      console.log('pins array', pins);
+    })
+    .catch((err) => reject(err));
+});
+
+// const getPins = () => axios.get(`${baseUrl}/pins.json`);
 
 export default { getPins };
