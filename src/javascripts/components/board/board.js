7c0e8d3a72c4659312e38d8f9d45a58e286084af
delindell <daviselindell@gmail.com>
2020-04-02 20:37:19 -0500

    can delete all the pins with board deletion as well, Successgit add .

--- src/javascripts/components/board/board.js
+++ src/javascripts/components/board/board.js
@@ -1,10 +1,18 @@
 import utils from '../../helpers/utils';
 import boardData from '../../helpers/data/boardData';
+import pinData from '../../helpers/data/pinData';
 
 const removeBoard = (e) => {
   const boardId = e.target.closest('.card').id;
   boardData.deleteBoard(boardId)
     .then(() => {
+      pinData.getPins(boardId).then((allPins) => {
+        allPins.forEach((pin) => {
+          if (boardId === pin.boardId) {
+            pinData.deletePin(pin.id);
+          }
+        });
+      });
       // eslint-disable-next-line no-use-before-define
       buildBoards();
     })
